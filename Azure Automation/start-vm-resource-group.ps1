9f97711e6f9c5bea324fd1aef5aac54bab3c11ba
sedelrio <sebastid@microsoft.com>
2018-04-05 07:42:14 -0300

    Add start VM automation

---
+++ Azure Automation/start-vm-resource-group.ps1
@@ -0,0 +1,22 @@
+﻿workflow Start
+{ 
+#    $day = (Get-Date).DayOfWeek
+#    if ($day -eq 'Saturday' -or $day -eq 'Sunday'){
+#    exit
+#   }  
+ 
+# Grupo de recursos a utilizar
+$ResourceGroupName = "RGName";
+
+#Conexion hacia la subscripcion de Azure mediante AzureRunasAccount
+$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint     
+     
+$vms = Get-AzureRmVM -ResourceGroupName $ResourceGroupName; 
+     
+    Foreach -Parallel ($vm in $vms){ 
+            Write-Output "Starting $($vm.Name)";         
+            Start-AzureRmVm -Name $vm.Name -ResourceGroupName $ResourceGroupName                      
+    } 
+}
+
